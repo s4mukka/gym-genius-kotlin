9b49d936b99ffd0eefad3e9ad37c005323459267
s4mukka <meninosam197@gmail.com>
2023-07-04 13:53:03 -0300

    add workouts ativity

---
+++ frontend/app/src/main/java/com/example/gymgenius/WorkoutsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gymgenius
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkoutsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workouts)
+    }
+}
